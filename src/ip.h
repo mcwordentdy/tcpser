707513db799ec1d0576bea5e4778b2533e00464c
geneb <geneb@deltasoft.com>
2016-03-02 07:46:50 -0800

    Initial upload

---
+++ src/ip.h
@@ -0,0 +1,26 @@
+/********************************************************************************/
+/* History:
+ *
+ * 20Jan14 gwb Added support for an ip address to be passed to ip_init_server_conn()
+ *             in support of the expanded "-p" command line option.
+ *
+ */
+
+#ifndef IP_H
+#define IP_H
+
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif
+
+int ip_init(void);
+int ip_init_server_conn(char ip_addr[], int port);
+int ip_connect(unsigned char addy[]);
+int ip_accept(int sSocket);
+int ip_disconnect(int fd);
+int ip_write(int fd,unsigned char* data,int len);
+int ip_read(int fd, unsigned char* data, int len);
+
+#endif
+
